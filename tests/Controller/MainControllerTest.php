9be66fb0af1e5fc14e4b9e39e38789f477fde561
Mateusz Labud <mateuszwwe999@interia.pl>
2023-01-19 23:39:25 +0100

    Pozostało napisac testy

---
+++ tests/Controller/MainControllerTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use App\Controller\MainController;
+use PHPUnit\Framework\TestCase;
+
+class MainControllerTest extends TestCase
+{
+    public function testCalculate()
+    {
+
+    }
+
+
+    public function testIndex()
+    {
+
+    }
+}
